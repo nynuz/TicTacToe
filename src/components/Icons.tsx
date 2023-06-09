1cbef0cf5c30353e23089b7aa6b7465b049789ae
nynuz <antonio.scibilia@gmail.com>
2023-06-09 15:28:26 +0200

    Fix some bugs

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -12,10 +12,8 @@ const Icons = ({name} : IconProps) => {
     switch (name) {
         case 'circle':
             return <Icon name="circle-thin" size={38} color="#F7CD2E" />
-            break;
         case 'cross':
             return <Icon name="times" size={38} color="#38CC77" />
-            break;
         
         default:
             return <Icon name="pencil" size={38} color="#0D0D0D" />
